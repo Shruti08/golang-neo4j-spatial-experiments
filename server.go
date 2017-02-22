cd60a659f66edd1a49d8888a9016a14e1781dc82
Shruti08 <shrutiwadnerkar@outlook.com>
2017-02-22 20:53:17 +0530

    Implemented Create User with Spatial Indexing

--- server.go
+++ server.go
@@ -46,10 +46,10 @@ func createNode(jsonBody map[string]string) bool {
 	defer db.Close()
 
 	stmt, err := db.Prepare(`CREATE (user:User {0})
-				 WITH count(*) as dummy
-				 MATCH(n:User) SET n.lat = toFloat(n.lat),n.lon=toFloat(n.lon)
-				 WITH count(*) as dummy
-	                         MATCH (n:User) with n CALL spatial.addNode('geoLocation',n) YIELD node RETURN node;
+				 WITH count(*) AS dummy
+				 MATCH(n:User) WHERE n.uid = {1} SET n.lat = toFloat(n.lat),n.lon=toFloat(n.lon)
+				 WITH count(*) AS dummy
+	                         MATCH (n:User) WHERE n.uid = {2} WITH n CALL spatial.addNode('geoLocation',n) YIELD node RETURN node;
 	                         `)
 
 	if err != nil {
@@ -58,7 +58,7 @@ func createNode(jsonBody map[string]string) bool {
 	}
 	defer stmt.Close()
 
-	rows, err := stmt.Exec(jsonBody)
+	rows, err := stmt.Exec(jsonBody,jsonBody["uid"],jsonBody["uid"])
 
 	if err != nil {
 		logMessage(methodSource + "Error Adding Parameters.Desc: " + err.Error())
