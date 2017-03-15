71d199186d2b1593ca8705b1707a6d30efc7e1af
Shruti08 <shrutiwadnerkar@outlook.com>
2017-03-16 00:00:38 +0530

    Added Sample for Golang Authentication

--- server.go
+++ server.go
@@ -5,21 +5,58 @@ import (
 	"github.com/labstack/echo"
 	"realworld/controllers"
 	_ "gopkg.in/cq.v1"
+	"time"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo/middleware"
 )
+
+func getMeIn(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	if username == "jon" && password == "shhh!" {
+		// Create token
+		token := jwt.New(jwt.SigningMethodHS256)
+
+		// Set claims
+		claims := token.Claims.(jwt.MapClaims)
+		claims["name"] = "Jon Snow"
+		claims["admin"] = true
+		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+		// Generate encoded token and send it as response.
+		t, err := token.SignedString([]byte("secret"))
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, map[string]string{
+			"token": t,
+		})
+	}
+
+	return echo.ErrUnauthorized
+}
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "HELLO FROM API")
 	})
-	e.POST("/createUser", controllers.CreateUser)
-	e.POST("/checkUserLogin", controllers.CheckUserLogin)
-	e.POST("/addUserInterests", controllers.CreateAddInterests)
-	e.POST("/getUserInterests", controllers.FetchInterests)
-	e.POST("/sendConnectionReq", controllers.SendConnectionRequest)
-	e.POST("/acceptConnectionReq", controllers.AcceptConnectionRequest)
-	e.POST("/blockUser", controllers.BlockUser)
-	e.POST("/unblockUser", controllers.UnBlockUser)
-	e.POST("/getSimilarUsers", controllers.FetchSimilarUsers)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.POST("/letsGetIn", getMeIn)
+	r := e.Group("/api")
+	r.Use(middleware.JWT([]byte("secret")))
+
+
+	r.POST("/createUser", controllers.CreateUser)
+	r.POST("/checkUserLogin", controllers.CheckUserLogin)
+	r.POST("/addUserInterests", controllers.CreateAddInterests)
+	r.POST("/getUserInterests", controllers.FetchInterests)
+	r.POST("/sendConnectionReq", controllers.SendConnectionRequest)
+	r.POST("/acceptConnectionReq", controllers.AcceptConnectionRequest)
+	r.POST("/blockUser", controllers.BlockUser)
+	r.POST("/unblockUser", controllers.UnBlockUser)
+	r.POST("/getSimilarUsers", controllers.FetchSimilarUsers)
 	e.Logger.Fatal(e.Start(":8000"))
 }
